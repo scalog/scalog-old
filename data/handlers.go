d9dbacae751487ad6138c5c8737e68f335238bb9
Evan Zhao <etz3@cornell.edu>
2019-02-28 15:27:46 -0500

    Data layer logic implemented
    Core structures for storing and transferring records sent to the data
    layer are added in this commit. Not included in this commit are
    committed cut ordering logic and kubernetes configs.

--- data/handlers.go
+++ data/handlers.go
@@ -2,27 +2,115 @@ package data
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"sync"
 
+	"github.com/spf13/viper"
+
+	"github.com/scalog/data/filesystem"
+	"github.com/scalog/data/messaging"
 	pb "github.com/scalog/data/messaging"
 )
 
-// We should mount some persistent volume. For now the current plan is just to start running a small sql
-// database so that querying is simple.
-
-type dataServer struct {
+// Record is a pending message to the ordering layer
+type Record struct {
+	cid        int32
+	csn        int32
+	record     string
+	commitResp chan int // Should send back a GSN
 }
 
-// #TODO Figure out if creating a mysql instance everywhere is efficient
-func (s *dataServer) Append(context.Context, *pb.AppendRequest) (*pb.AppendResponse, error) {
-	return nil, nil
+// We should mount some persistent volume. the filesystem package will be used to write
+// to this mounted volume.
+type dataServer struct {
+	// Storage for this server in particular
+	stableStorage *filesystem.RecordStorage
+
+	// Main storage stacks for incoming records
+	// TODO: evantzhao make this faster by using a separate go routine for each
+	// server stack.
+	serverBuffers [][]Record
+
+	// lastCommitedRecords stores the integer value of the last commited
+	// value in each server buffer
+	lastCommitedRecords []int
+
+	// TODO: evantzhao make this more efficient by either utilizing mutexes for
+	// each server log. Also consider channel
+	// Protects all internal structures
+	mu sync.Mutex
+
+	// Items written to these channels will be sent to these corresponding pods
+	shardServers []chan messaging.ReplicateRequest
+}
+
+// Append adds the specified record to the local data server, and this server additionally
+// forwards the append request to other servers within the shard with Replicate. Responds
+// to the client with a global sequence number when the Ordering layer commits
+// this record.
+func (s *dataServer) Append(c context.Context, req *pb.AppendRequest) (*pb.AppendResponse, error) {
+	s.mu.Lock()
+	r := &Record{
+		cid:        req.Cid,
+		csn:        req.Csn,
+		record:     req.Record,
+		commitResp: make(chan int),
+	}
+	s.serverBuffers[viper.GetInt("id")] = append(s.serverBuffers[viper.GetInt("id")], *r)
+	s.mu.Unlock()
+
+	replicateRequest := &messaging.ReplicateRequest{
+		ServerID: viper.GetInt32("id"),
+		Record:   req.Record,
+	}
+	for idx, ch := range s.shardServers {
+		if idx == viper.GetInt("id") {
+			continue
+		}
+		ch <- *replicateRequest
+	}
+	gsn := int32(<-r.commitResp)
+	return &pb.AppendResponse{Csn: r.csn, Gsn: gsn}, nil
 }
 
 func (s *dataServer) Replicate(stream pb.Data_ReplicateServer) error {
-	return nil
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		s.mu.Lock()
+		s.serverBuffers[in.ServerID] = append(s.serverBuffers[in.ServerID], Record{record: in.Record})
+		s.mu.Unlock()
+	}
 }
 
 func (s *dataServer) Commit(stream pb.Data_CommitServer) error {
-	return nil
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		if len(in.Cut) != len(s.serverBuffers) {
+			panic(
+				fmt.Sprintf("[Data] Received cut is of irregular length (%d vs %d)",
+					len(in.Cut),
+					len(s.serverBuffers),
+				),
+			)
+		}
+
+		// TODO evanzhao: implement cut update logic. Should push GSN's to record channel
+	}
 }
 
 func (s *dataServer) Subscribe(*pb.SubscribeRequest, pb.Data_SubscribeServer) error {
--- data/main.go
+++ data/main.go
@@ -1,9 +1,16 @@
 package data
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
+	"sync"
+
+	"github.com/scalog/data/filesystem"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 
 	"github.com/scalog/data/messaging"
 	"github.com/scalog/logger"
@@ -12,17 +19,77 @@ import (
 	"google.golang.org/grpc"
 )
 
-// We should mount some persistent volume. For now the current plan is just to start running a small sql
-// database so that querying is simple.
+// Should form a channel which writes to a specific pod on a specific pod ip
+func podConn(podIP string, ch chan messaging.ReplicateRequest) {
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithInsecure())
 
-// #TODO Figure out if creating a mysql instance everywhere is efficient
+	conn, err := grpc.Dial(podIP+":"+viper.GetString("port"), opts...)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	client := messaging.NewDataClient(conn)
+	stream, err := client.Replicate(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	for req := range ch {
+		if err := stream.Send(&req); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func initKubernetesClient() *kubernetes.Clientset {
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	return clientset
+}
 
 func newDataServer() *dataServer {
-	s := &dataServer{}
+	clientset := initKubernetesClient()
+
+	listOptions := metav1.ListOptions{
+		LabelSelector: viper.GetString("shard"),
+	}
+	// Recover all servers within the shard
+	pods, err := clientset.CoreV1().Pods("").List(listOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	// Setup pod communication channels
+	var shardPods []chan messaging.ReplicateRequest
+	for idx, pod := range pods.Items {
+		if idx == viper.GetInt("id") {
+			shardPods = append(shardPods, nil)
+			continue
+		}
+		shardPods = append(shardPods, make(chan messaging.ReplicateRequest))
+		go podConn(pod.Status.PodIP, shardPods[len(shardPods)-1])
+	}
+
+	fs := filesystem.New("tmp")
+	s := &dataServer{
+		stableStorage: &fs,
+		serverBuffers: make([][]Record, viper.GetInt("serverCount")),
+		mu:            sync.Mutex{},
+		shardServers:  shardPods,
+	}
 	return s
 }
 
-// Start data server
+// Start RPC server
 func Start() {
 	logger.Printf("Data layer server %d started on %d\n", viper.Get("id"), viper.Get("port"))
 	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", viper.Get("port")))
