c20770fa1aba04e2d6752f90deaa1147bc63e644
David Chu <dc788@cornell.edu>
2019-03-03 22:11:13 -0500

    raft wrapper creation

--- order/raft.go
+++ order/raft.go
@@ -1,16 +1,41 @@
 package order
 
 import (
-	"github.com/coreos/etcd/raft/raftpb"
 	"go.etcd.io/etcd/raft"
 )
 
 type raftNode struct {
 	incoming <-chan string
 	outgoing <-chan string
+	config   raft.Config
+	node     raft.Node
+}
 
-	configuration raftpb.ConfState
+func newRaftNode(id uint64, peers []uint64, incoming <-chan string, outgoing <-chan string) raftNode {
+	storage := raft.NewMemoryStorage()
 
-	node    raft.Node
-	storage *raft.MemoryStorage
+	config := raft.Config{
+		ID:              id,
+		ElectionTick:    10,
+		HeartbeatTick:   1,
+		Storage:         storage,
+		MaxSizePerMsg:   4096,
+		MaxInflightMsgs: 256,
+	}
+
+	peersObject := make([]raft.Peer, len(peers))
+	for i := range peers {
+		peersObject[i] = raft.Peer{ID: peers[i]}
+	}
+
+	node := raft.StartNode(&config, peersObject)
+
+	nodeWrapper := raftNode{
+		incoming,
+		outgoing,
+		config,
+		node,
+	}
+
+	return nodeWrapper
 }
