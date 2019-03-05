63e06209570cc7a9cd1ceb081ba217a0fee35fac
David Chu <dc788@cornell.edu>
2019-03-05 00:44:29 -0500

    Beginning logic for order layer

--- order/handlers.go
+++ order/handlers.go
@@ -5,7 +5,42 @@ import (
 	pb "github.com/scalog/scalog/order/messaging"
 )
 
+// Server's view of latest cuts in all servers of the shard
+type ShardCut []int
+
+// Map<Shard ID, Shard cuts reported by all servers in shard>
+type ContestedGlobalCut map[string][]ShardCut
+
+// Map<Shard ID, Cuts in the shard>
+type CommittedGlobalCut map[string]ShardCut
+
 type orderServer struct {
+	committedGlobalCut CommittedGlobalCut
+	contestedGlobalCut ContestedGlobalCut
+	globalSequenceNum  int
+}
+
+func initGlobalCut(shardIds []string, numServersPerShard int) ContestedGlobalCut {
+	cut := make(ContestedGlobalCut, len(shardIds))
+	for _, shardId := range shardIds {
+		shardCuts := make([]ShardCut, numServersPerShard, numServersPerShard)
+		for i := 0; i < numServersPerShard; i++ {
+			shardCuts[i] = make(ShardCut, numServersPerShard, numServersPerShard)
+		}
+		cut[shardId] = shardCuts
+	}
+	return cut
+}
+
+/**
+Process new cut from data server.
+
+cut: Server's view of the latest cut
+shardId: Id of shard that server belongs to
+num: Index of server in shard
+*/
+func (gCut ContestedGlobalCut) newShardCut(cut ShardCut, shardId string, num int) {
+
 }
 
 func (s *orderServer) Report(ctx context.Context, req *pb.ReportRequest) (*pb.ReportResponse, error) {
