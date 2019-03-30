9d30bb9d8105d08258b17e6c5f114dd4f5f97dc9
Evan Zhao <etz3@cornell.edu>
2019-03-30 18:32:53 -0400

    Created discovery command
    This launches a new server for the express purpose of responding to clients with a list of available data servers that they can potentially talk to. After dockerizing, this should live as a deployment, where this discovery service is replicated behind the scenes, allowing for a reliable gateway to the Scalog service.

--- cmd/discovery.go
+++ cmd/discovery.go
@@ -17,6 +17,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/scalog/scalog/discovery"
 	"github.com/spf13/cobra"
 )
 
@@ -32,6 +33,7 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("discovery called")
+		discovery.Start()
 	},
 }
 
--- discovery/main.go
+++ discovery/main.go
@@ -1,11 +1,85 @@
 package discovery
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/scalog/scalog/discovery/rpc"
 	"github.com/scalog/scalog/logger"
 	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 // Start serving requests for the data layer
 func Start() {
 	logger.Printf("Discovery server starting on %d\n", viper.Get("port"))
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", viper.Get("port")))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	rpc.RegisterDiscoveryServer(grpcServer, newDiscoveryServer())
+	logger.Printf("Discovery server available on %d\n", viper.Get("port"))
+	grpcServer.Serve(lis)
+}
+
+type discoveryServer struct {
+	client       *kubernetes.Clientset
+	serverLabels metav1.ListOptions
+}
+
+func (ds *discoveryServer) DiscoverServers(ctx context.Context, req *rpc.DiscoverRequest) (*rpc.DiscoverResponse, error) {
+	services, err := ds.client.CoreV1().Services(viper.GetString("namespace")).List(ds.serverLabels)
+	if err != nil {
+		logger.Panicf("Error when querying kubernetes api for list of data service IPs")
+	}
+
+	serviceIPs := make([]string, len(services.Items))
+	for i, service := range services.Items {
+		// We assume that each one of these services only has one external IP
+		var externalIPs []string
+		if len(service.Spec.ExternalIPs) == 0 {
+			return nil, errors.New("External data service does not have an external IP")
+		}
+		serviceIPs[i] = externalIPs[0]
+	}
+
+	resp := rpc.DiscoverResponse{
+		ServerAddresses: serviceIPs,
+	}
+	return &resp, nil
+}
+
+func newDiscoveryServer() *discoveryServer {
+	clientset := initKubernetesClient()
+	listOptions := metav1.ListOptions{
+		LabelSelector: "type=exposedDataService",
+	}
+	ds := &discoveryServer{
+		client:       clientset,
+		serverLabels: listOptions,
+	}
+	return ds
+}
+
+//////////////////	Utility Functions
+
+func initKubernetesClient() *kubernetes.Clientset {
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	return clientset
 }
