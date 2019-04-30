a0d3254dcf45ca33131154e03aea67a1435d4bc3
Evan Zhao <etz3@cornell.edu>
2019-04-30 00:33:36 -0400

    User permissions

--- install_scripts/install-scalog-deps.sh
+++ install_scripts/install-scalog-deps.sh
@@ -19,21 +19,6 @@ sudo add-apt-repository \
 ## Install Docker CE.
 sudo apt-get update && sudo apt-get install docker-ce=18.06.2~ce~3-0~ubuntu
 
-# # Setup daemon.
-# sudo cat > /etc/docker/daemon.json <<EOF
-# {
-#   "exec-opts": ["native.cgroupdriver=systemd"],
-#   "log-driver": "json-file",
-#   "log-opts": {
-#     "max-size": "100m"
-#   },
-#   "storage-driver": "overlay2"
-# }
-# EOF
-
-# # Restart docker.
-# sudo systemctl daemon-reload
-
 echo "Installing kubeadm on this local machine..."
 
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
@@ -44,6 +29,7 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
-echo "Successfully installed kubeadm"
+modprobe br_netfilter
+sudo swapoff -a 
 
-wget -O - https://raw.githubusercontent.com/scalog/scalog/master/install_scripts/install-scalog-deps.sh | bash
+echo "Successfully installed kubeadm"
