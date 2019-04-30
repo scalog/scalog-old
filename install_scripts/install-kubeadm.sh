8464d163bbcf23820bee674e5eb105a304777d11
Evan Zhao <etz3@cornell.edu>
2019-04-29 23:16:58 -0400

    Add install scripts for kubeadm

---
+++ install_scripts/install-kubeadm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Installing kubeadm on this local machine"
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+echo "Success :^)"
