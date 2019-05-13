8706325cf09dddd2280545a61a2b29a217b723c4
Evan Zhao <etz3@cornell.edu>
2019-05-12 22:33:43 -0400

    [Bootstrap]: Permanently add admin.conf prof to .kube dir

--- install_scripts/bootstrap.sh
+++ install_scripts/bootstrap.sh
@@ -6,9 +6,10 @@ echo "Bootstrapping kubernetes cluster..."
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 
 # Change permissions, so kubectl can run as a normal user
-sudo cp /etc/kubernetes/admin.conf $HOME/
-sudo chown $(id -u):$(id -g) $HOME/admin.conf
-export KUBECONFIG=$HOME/admin.conf
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Create Pod Network Layer
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/a70459be0084506e4ec919aa1c114638878db11b/Documentation/kube-flannel.yml
+  
