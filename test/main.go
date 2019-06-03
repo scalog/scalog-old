9192fcdfef9c0dab30685f339dffaa0323e6d9d4
Cong Ding <tuxding@gmail.com>
2019-06-03 13:17:35 -0400

    test: messaging -> pb

--- test/main.go
+++ test/main.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/scalog/scalog/data/messaging"
+	"github.com/scalog/scalog/data/datapb"
 	"github.com/scalog/scalog/data/storage"
-	"github.com/scalog/scalog/discovery/rpc"
+	"github.com/scalog/scalog/discovery/discpb"
 	"google.golang.org/grpc"
 )
 
@@ -32,8 +32,8 @@ func testGRPC(port string) {
 		panic(err)
 	}
 	defer conn.Close()
-	client := messaging.NewDataClient(conn)
-	appendRequest := &messaging.AppendRequest{Cid: 1, Csn: 1, Record: "Hello World"}
+	client := datapb.NewDataClient(conn)
+	appendRequest := &datapb.AppendRequest{Cid: 1, Csn: 1, Record: "Hello World"}
 
 	_, error := client.Append(context.Background(), appendRequest)
 	if error != nil {
@@ -50,8 +50,8 @@ func connectToLiveCluster(address string) {
 		panic(err)
 	}
 
-	client := rpc.NewDiscoveryClient(conn)
-	appendRequest := &rpc.DiscoverRequest{}
+	client := discpb.NewDiscoveryClient(conn)
+	appendRequest := &discpb.DiscoverRequest{}
 
 	resp, error := client.DiscoverServers(context.Background(), appendRequest)
 	if error != nil {
@@ -76,10 +76,10 @@ func appendToShard(address string) {
 	}
 	fmt.Println("Finished dialing.")
 
-	client := messaging.NewDataClient(conn)
+	client := datapb.NewDataClient(conn)
 
 	for i := 1; i < 100; i++ {
-		appendRequest := &messaging.AppendRequest{Cid: 53325, Csn: int32(i), Record: "please work :'("}
+		appendRequest := &datapb.AppendRequest{Cid: 53325, Csn: int32(i), Record: "please work :'("}
 		resp, error := client.Append(context.Background(), appendRequest)
 		if error != nil {
 			panic(error)
